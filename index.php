e3b025380cae53ff972172028d0b97046685b3da
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-10-29 15:05:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,14 @@ $sql = "SELECT * FROM pracownicy";
 $result = mysqli_query($conn, $sql);
 
 echo('<table border="1">');
-echo('<th>Imie</th><th>zarobki</th>');
+    echo('<th>Imie</th><th>zarobki</th>');
 
-while($row=mysqli_fetch_assoc($result))
-{
-  echo('<tr>');
-  echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>');
-}
+    while($row=mysqli_fetch_assoc($result))
+    {
+        echo('<tr>');
+        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('</tr>');
+    }
 
     echo('</table>');
 ?>
