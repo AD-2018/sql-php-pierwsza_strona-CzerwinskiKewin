81ae8af0da83cc8d453615498db687ac9cb4498c
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-10-29 14:41:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
-echo("Witaj swiecie!");
-echo("Kewin Czerwinski jestem");
+$conn = new mysqli("sql7.freemysqlhosting.net","sql7373394","35p8b2TQdP","sql7373394");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+echo('<table border="1">');
+echo('<th>Imie</th><th>zarobki</th>');
+
+while($wiersz=mysqli_fetch_assoc($wynik))
+{
+  echo('<tr>');
+  echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+  echo('</tr>');
+}
+
+    echo('</table>');
 ?>
