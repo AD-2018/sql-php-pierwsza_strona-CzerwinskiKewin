6ebc85eb0a12d81b2434986c7070b7861c76122d
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-10-29 15:02:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
-$servername = "sql7.freemysqlhosting.net";
-$username = "sql7373394";
-$password = "35p8b2TQdP";
-$dbname = "sql7373394#;
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-$sql = "SELECT * FROM pracownicy";
-$result = mysqli_query($conn, $sql);
-
-echo('<table border="1">');
-echo('<th>Imie</th><th>zarobki</th>');
-
-while($row=mysqli_fetch_assoc($result))
-{
-  echo('<tr>');
-  echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>');
-}
-
-    echo('</table>');
+echo("hh");
 ?>
