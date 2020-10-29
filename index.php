d09d7367fe345489966a937d6108027d3328a96d
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-10-29 14:51:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
-$conn = new mysqli("sql7.freemysqlhosting.net","sql7373394","35p8b2TQdP","sql7373394");
+$servername = "sql7.freemysqlhosting.net";
+$username = "sql7373394";
+$password = "35p8b2TQdP";
+$dbname = "sql7373394#;
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 $sql = "SELECT * FROM pracownicy";
-$wynik = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
 
 echo('<table border="1">');
 echo('<th>Imie</th><th>zarobki</th>');
 
-while($wiersz=mysqli_fetch_assoc($wynik))
+while($row=mysqli_fetch_assoc($result))
 {
   echo('<tr>');
-  echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-  echo('</tr>');
+  echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['dzial'].'</td><td>'.$row['zarobki'].'</td><td>'.$row['data_urodzenia'].'</td>');
 }
 
     echo('</table>');
