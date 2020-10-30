63a43ef0cfb7c2a23426b0c98912dbed377fac5d
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-10-30 12:11:43 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,12 @@
 <?php
+$servername = "sql7.freemysqlhosting.net";
+$username = "sql7373394";
+$password = "35p8b2TQdP";
+$dbname = "sql7373394";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
