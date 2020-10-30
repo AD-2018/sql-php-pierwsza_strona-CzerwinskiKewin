5e04d3a3b9ecd29a286ff6c5c283600a87b19dca
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-10-30 11:49:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
 $conn = new mysqli("sql7.freemysqlhosting.net","sql7373394","35p8b2TQdP","sql7373394");
+
+if ($conn->connect_error) 
+{
+  die("Connection failed: " . $conn->connect_error);
+}
+
 $sql = "SELECT * FROM pracownicy";
 $wynik = mysqli_query($conn, $sql);
 
