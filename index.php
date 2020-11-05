eda4c9169afffeb30763c32b8eb152716f600358
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-11-05 14:00:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,9 @@ $wynik = mysqli_query($conn, $sql);
 
     echo('</table>');
     
+    
+$sql = "SELECT * FROM pracownicy where imie like '%a'";
+$wynik = mysqli_query($conn, $sql);
     echo("Zadanie 2");
     echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
@@ -36,7 +39,10 @@ $wynik = mysqli_query($conn, $sql);
     }
 
     echo('</table>');
+  
     
+$sql = "SELECT * FROM pracownicy where imie not like '%a'";
+$wynik = mysqli_query($conn, $sql);
     echo("Zadanie 3");
     echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
