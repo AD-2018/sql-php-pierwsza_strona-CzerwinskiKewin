35cac98ace477494903c78c1399677afb0f3da6a
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-11-05 14:52:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,108 +4,16 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<?php
+    
+echo("<h1>Kewin Czerwinski</h1>");
+    
+?>
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-CzerwinskiKewin">Github</a>
 <div class="diw">
     <a href="pracownicyOrganizacja.php">prac_org</a>
     <a href="funkcjeAgregujace.php">agr</a>
 </div>
-    
-<?php
-require_once("connect.php");
-
-$sql = "SELECT * FROM pracownicy,organizacja where id_org=dzial";
-$wynik = mysqli_query($conn, $sql);
-
-    echo("Zadanie 1");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    
-echo("<br>");
-    
-    
-$sql = "SELECT * FROM pracownicy where imie like '%a'";
-$wynik = mysqli_query($conn, $sql);
-    echo("Zadanie 2");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    
-echo("<br>");
-  
-    
-$sql = "SELECT * FROM pracownicy where imie not like '%a'";
-$wynik = mysqli_query($conn, $sql);
-    echo("Zadanie 3");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    
-echo("<br>");
-    
-    
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial";
-$wynik = mysqli_query($conn, $sql);
-
-    echo("Zadanie 4");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    
-echo("<br>");
-   
-    
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial";
-$wynik = mysqli_query($conn, $sql);
-
-    echo("Zadanie 5");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    
-echo("<br>");
-
-?>
     
 </body>
 </html>
