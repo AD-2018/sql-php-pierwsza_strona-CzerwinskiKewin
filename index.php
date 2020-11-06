1a007c376fcb777b8fa8ab0539f1e9cbfe7c4ee6
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-11-06 11:52:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,16 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
-    
-echo("<h1>Kewin Czerwinski</h1>");
-    
-?>
     
 <div class="diw">
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-CzerwinskiKewin">Github</a>
     <a href="pracownicyOrganizacja.php">prac_org</a>
     <a href="funkcjeAgregujace.php">agr</a>
 </div>
+    
+<?php
+    
+echo("<h1>Kewin Czerwinski</h1>");
     
 require_once("connect.php");
 
@@ -75,6 +74,7 @@ $wynik = mysqli_query($conn, $sql);
     echo('</table>');
     
 echo("<br>");
+?>
     
 </body>
 </html>
