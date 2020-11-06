9629f96f281c911c4fe825c642fe5e0a0a490e1c
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-11-06 12:16:01 +0100

    Update pracownicyOrganizacja.php

--- pracownicyOrganizacja.php
+++ pracownicyOrganizacja.php
@@ -7,7 +7,7 @@
     
 <div class="diw">
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-CzerwinskiKewin">Github</a>
-    <a href="pracownicyOrganizacja.php">Pracownicy i Organizacja</a>
+    <a href="index.php">Strona Główna</a>
     <a href="funkcjeAgregujace.php">Funkcje Agregujace</a>
 </div>
 <?php
