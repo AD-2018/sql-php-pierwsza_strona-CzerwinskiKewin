38fa9b83119c321ae78b7577bb8b3d63eb600225
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-11-06 12:06:59 +0100

    Update pracownicyOrganizacja.php

--- pracownicyOrganizacja.php
+++ pracownicyOrganizacja.php
@@ -1,62 +1,45 @@
 <?php
 require_once("connect.php");
-
-$sql = "SELECT * FROM pracownicy,organizacja where id_org=dzial";
+    
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial";
 $wynik = mysqli_query($conn, $sql);
 
     echo("Zadanie 1");
     echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
+    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
 
     while($wiersz=mysqli_fetch_assoc($wynik))
     {
         echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
     
 echo("<br>");
+   
     
-    
-$sql = "SELECT * FROM pracownicy where imie like '%a'";
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and (dzial=1 or dzial=4)";
 $wynik = mysqli_query($conn, $sql);
-    echo("Zadanie 2");
+
+    echo("Zadanie 5");
     echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
+    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
 
     while($wiersz=mysqli_fetch_assoc($wynik))
     {
         echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
     
 echo("<br>");
-  
-    
-$sql = "SELECT * FROM pracownicy where imie not like '%a'";
-$wynik = mysqli_query($conn, $sql);
-    echo("Zadanie 3");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
 
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
-        echo('</tr>');
-    }
 
-    echo('</table>');
-    
-echo("<br>");
-    
-    
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial";
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie like '%a'";
 $wynik = mysqli_query($conn, $sql);
 
     echo("Zadanie 4");
@@ -73,12 +56,109 @@ $wynik = mysqli_query($conn, $sql);
     echo('</table>');
     
 echo("<br>");
-   
-    
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial";
+
+
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie not like '%a'";
 $wynik = mysqli_query($conn, $sql);
 
-    echo("Zadanie 5");
+    echo("Zadanie 4");
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik))
+    {
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+echo("<br>");
+echo("<br>");
+echo("<h1>Sortowanie</h1>")
+
+
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial order by imie desc";
+$wynik = mysqli_query($conn, $sql);
+
+    echo("Zadanie sortowanie 1");
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik))
+    {
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+echo("<br>");
+
+
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and dzial=3 order by imie asc";
+$wynik = mysqli_query($conn, $sql);
+
+    echo("Zadanie sortowanie 2");
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik))
+    {
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+echo("<br>");
+
+
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie like '%a' order by zarobki asc";
+$wynik = mysqli_query($conn, $sql);
+
+    echo("Zadanie sortowanie 3");
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik))
+    {
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+echo("<br>");
+
+
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie like '%a' and (dzial=1 or dzial=3) order by zarobki asc";
+$wynik = mysqli_query($conn, $sql);
+
+    echo("Zadanie sortowanie 4");
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik))
+    {
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+echo("<br>");
+
+
+$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie not like '%a' and order by nazwa_dzial asc, zarobki asc";
+$wynik = mysqli_query($conn, $sql);
+
+    echo("Zadanie sortowanie 5");
     echo('<table border="1">');
     echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
 
