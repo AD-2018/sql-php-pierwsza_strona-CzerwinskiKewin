d658610edc4b25feaccbf357196195fc3f999298
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-11-12 12:02:20 +0100

    Update funkcjeAgregujace.php

--- funkcjeAgregujace.php
+++ funkcjeAgregujace.php
@@ -53,17 +53,17 @@ $wynik = mysqli_query($conn, $sql);
 echo("<br>");
 
 
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie like '%a'";
+$sql = "SELECT sum(zarobki) FROM pracownicy,organizacja where id_org=dzial and imie not like '%a' and (dzial=2 or dzial=3)";
 $wynik = mysqli_query($conn, $sql);
 
-    echo("Zadanie 3");
+    echo("Zadanie 1");
     echo('<table border="1">');
-    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+    echo('<th>Suma zarobków</th>');
 
     while($wiersz=mysqli_fetch_assoc($wynik))
     {
         echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('<td>'.$wiersz['sum(zarobki)'].'</td>');
         echo('</tr>');
     }
 
@@ -72,38 +72,17 @@ $wynik = mysqli_query($conn, $sql);
 echo("<br>");
 
 
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie not like '%a'";
+$sql = "SELECT avg(zarobki) FROM pracownicy,organizacja where id_org=dzial and imie not like '%a'";
 $wynik = mysqli_query($conn, $sql);
 
-    echo("Zadanie 4");
+    echo("Zadanie 1");
     echo('<table border="1">');
-    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+    echo('<th>Średnia zarobków</th>');
 
     while($wiersz=mysqli_fetch_assoc($wynik))
     {
         echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-    
-echo("<br>");
-echo("<br>");
-echo("<h1>Sortowanie</h1>");
-
-
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial order by imie desc";
-$wynik = mysqli_query($conn, $sql);
-
-    echo("Zadanie sortowanie 1");
-    echo('<table border="1">');
-    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
-
-    while($wiersz=mysqli_fetch_assoc($wynik))
-    {
-        echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('<td>'.$wiersz['avg(zarobki)'].'</td>');
         echo('</tr>');
     }
 
@@ -112,17 +91,17 @@ $wynik = mysqli_query($conn, $sql);
 echo("<br>");
 
 
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and dzial=3 order by imie asc";
+$sql = "SELECT avg(zarobki) FROM pracownicy,organizacja where id_org=dzial and dzial=4";
 $wynik = mysqli_query($conn, $sql);
 
-    echo("Zadanie sortowanie 2");
+    echo("Zadanie 1");
     echo('<table border="1">');
-    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+    echo('<th>Średnia zarobków</th>');
 
     while($wiersz=mysqli_fetch_assoc($wynik))
     {
         echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('<td>'.$wiersz['avg(zarobki)'].'</td>');
         echo('</tr>');
     }
 
@@ -131,17 +110,17 @@ $wynik = mysqli_query($conn, $sql);
 echo("<br>");
 
 
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie like '%a' order by zarobki asc";
+$sql = "SELECT avg(zarobki) FROM pracownicy,organizacja where id_org=dzial and imie not like '%a' and (dzial=1 or dzial=2)";
 $wynik = mysqli_query($conn, $sql);
 
-    echo("Zadanie sortowanie 3");
+    echo("Zadanie 1");
     echo('<table border="1">');
-    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+    echo('<th>Średnia zarobków</th>');
 
     while($wiersz=mysqli_fetch_assoc($wynik))
     {
         echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('<td>'.$wiersz['avg(zarobki)'].'</td>');
         echo('</tr>');
     }
 
@@ -150,17 +129,36 @@ $wynik = mysqli_query($conn, $sql);
 echo("<br>");
 
 
-$sql = "SELECT imie,nazwa_dzial,zarobki,data_urodzenia FROM pracownicy,organizacja where id_org=dzial and imie like '%a' and (dzial=1 or dzial=3) order by zarobki asc";
+$sql = "SELECT count(imie) FROM pracownicy,organizacja where id_org=dzial";
 $wynik = mysqli_query($conn, $sql);
 
-    echo("Zadanie sortowanie 4");
+    echo("Zadanie 1");
     echo('<table border="1">');
-    echo('<th>Imie</th><th>nazwa_dzial</th><th>zarobki</th><th>data_urodzenia</th>');
+    echo('<th>Liczba pracowników</th>');
 
     while($wiersz=mysqli_fetch_assoc($wynik))
     {
         echo('<tr>');
-        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['nazwa_dzial'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>'.'<td>'.$wiersz['data_urodzenia'].'</td>');
+        echo('<td>'.$wiersz['count(imie)'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+    
+echo("<br>");
+
+
+$sql = "SELECT count(imie) FROM pracownicy,organizacja where id_org=dzial and imie like '%a' and (dzial=1 or dzial=3)";
+$wynik = mysqli_query($conn, $sql);
+
+    echo("Zadanie 1");
+    echo('<table border="1">');
+    echo('<th>Liczba pracowników</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik))
+    {
+        echo('<tr>');
+        echo('<td>'.$wiersz['count(imie)'].'</td>');
         echo('</tr>');
     }
 
