92a3655e7ee21d569bfc769cff90e6e5c6c3fd65
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-11-12 15:02:48 +0100

    Create dataCzas.php

---
+++ dataCzas.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<div class="diw">
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-CzerwinskiKewin">Github</a>
+    <a href="pracownicyOrganizacja.php">Pracownicy i Organizacja</a>
+    <a href="index.php">Strona Główna</a>
+</div>
+
+<?php
+
+
+
+
+?>
+
+</body>
+</html>
