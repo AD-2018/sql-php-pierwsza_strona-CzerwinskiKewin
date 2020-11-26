11a8e2e403cf4815e42c2ab898d379d87a833acb
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-11-26 13:40:46 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,18 +4,24 @@ $username = "kcz";
 $password = "zaq1@WSX";
 $dbname = "kcz_20";
 
+<?php
+echo("jestes w insert.php");
+echo "<li>".$_POST['imie'];
+echo "<li>".$_POST['dzial'];
+echo "<li>".$_POST['zarobki'];
+echo "<li>".$_POST['data_urodzenia'];
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null, $_POST['name'], 3, 36,'1995-10-21')";
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
-//informacja o ewentualnych błędach
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
