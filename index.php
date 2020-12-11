d534fb4aa6ad2f80059cdd9bcaf5edead304b835
Czerwinski-Kewin <73647814+Czerwinski-Kewin@users.noreply.github.com>
2020-12-11 13:07:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,10 +17,9 @@ echo('</div>');
     
 echo('<div class="linki">'); 
     echo('<div class="diw">');
-        echo('<a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-CzerwinskiKewin">Github</a>');
-        echo("<br>");
-        echo("<br>");
+        echo('<a class="link" href="https://github.com/AD-2018/sql-php-pierwsza_strona-CzerwinskiKewin">Github</a><br><br>');
         echo('<a class="link" href="pracownicyOrganizacja.php">Pracownicy i Organizacja</a>');
+        echo("<br>");
         echo("<br>");
         echo("<br>");
         echo('<a class="link" href="funkcjeAgregujace.php">Funkcje Agregujące</a>');
