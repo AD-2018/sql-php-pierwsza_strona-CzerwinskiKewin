baa7a4f90d29a76677953ec6db6ac1b6bb9a96b3
Czerwinski-Kewin <czerwinski.kewin1@gmail.com>
2021-02-22 09:51:43 +0100

    connect

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "mysql-kcz.alwaysdata.net";
-$username = "kcz";
-$password = "zaq1@WSX";
-$dbname = "kcz_20";
+$servername = $_SERVER["servername"];
+$username = $_SERVER["username"];
+$password = $_SERVER["password"];
+$dbname = $_SERVER["dbname"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
