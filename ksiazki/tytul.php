ad2c0acf3e2b2c62bed9e7377a242d337586de37
Czerwinski-Kewin <czerwinski.kewin1@gmail.com>
2021-02-22 09:27:55 +0100

    tinsert

---
+++ ksiazki/tytul.php
@@ -0,0 +1,25 @@
+<?php
+echo("jestes w tytul.php <br>");
+echo $_POST['tytul'];
+
+require_once("../connect.php");
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "INSERT INTO bibl_autor(id_tytul, tytul) VALUES(null, '".$_POST['tytul']."');
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "dodano";
+  header('Location: https://znw.herokuapp.com/ksiazki/biblioteka.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
