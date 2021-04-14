18baf72362abc546415e8ef0b2c56c3c2a412cc8
Czerwinski-Kewin <czerwinski.kewin1@gmail.com>
2021-04-14 14:01:51 +0200

    rpg3

--- cssgrid/grid3/index.php
+++ cssgrid/grid3/index.php
@@ -30,20 +30,20 @@
       <header>
       <?php
         require_once("../../connect.php");
-        $sql = "SELECT * FROM sys_osoba";
+        $sql = "SELECT id_system, osoba, rola FROM sys_system, sys_osoba, sys_rola WHERE sys_osoba.id_osoba = sys_system.id_osoba AND sys_rola.id_rola = sys_system.id_rola order by id_system ASC";
         $wynik = mysqli_query($conn, $sql);
             
             echo("<br>");
-            echo("Osoby");
+            echo("System informatyczny");
             echo("<br>");
             echo($sql);
             echo('<table border="1">');
-            echo('<th>ID osoby</th><th>Osoba</th>');
+            echo('<th>ID</th><th>Osoba</th><th>Rola</th>');
 
             while($wiersz=mysqli_fetch_assoc($wynik))
             {
                 echo('<tr>');
-                echo('<td>'.$wiersz['id_osoba'].'</td>'.'<td>'.$wiersz['osoba'].'</td>');
+                echo('<td>'.$wiersz['id_system'].'</td>'.'<td>'.$wiersz['osoba'].'</td>'.'<td>'.$wiersz['rola'].'</td>');
                 echo('</tr>');
             }
 
@@ -87,20 +87,20 @@
       <footer>
       <?php
         require_once("../../connect.php");
-        $sql = "SELECT id_system, osoba, rola FROM sys_system, sys_osoba, sys_rola WHERE sys_osoba.id_osoba = sys_system.id_osoba AND sys_rola.id_rola = sys_system.id_rola order by id_system ASC";
+        $sql = "SELECT * FROM sys_osoba";
         $wynik = mysqli_query($conn, $sql);
             
             echo("<br>");
-            echo("System informatyczny");
+            echo("Osoby");
             echo("<br>");
             echo($sql);
             echo('<table border="1">');
-            echo('<th>ID</th><th>Osoba</th><th>Rola</th>');
+            echo('<th>ID osoby</th><th>Osoba</th>');
 
             while($wiersz=mysqli_fetch_assoc($wynik))
             {
                 echo('<tr>');
-                echo('<td>'.$wiersz['id_system'].'</td>'.'<td>'.$wiersz['osoba'].'</td>'.'<td>'.$wiersz['rola'].'</td>');
+                echo('<td>'.$wiersz['id_osoba'].'</td>'.'<td>'.$wiersz['osoba'].'</td>');
                 echo('</tr>');
             }
 
