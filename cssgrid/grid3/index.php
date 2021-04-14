5c95798ab5098b1386c65ad295646ce4bc4ded2a
Czerwinski-Kewin <czerwinski.kewin1@gmail.com>
2021-04-14 13:43:59 +0200

    link17

--- cssgrid/grid3/index.php
+++ cssgrid/grid3/index.php
@@ -13,19 +13,19 @@
               <br>
               <br>
               <br>
-              <a class="link" href="/index.php">Menu</a>
-              <a class="link" href="grid1/index.php">Grid 1</a><br><br>
-              <a class="link" href="grid2/index.php">Grid 2</a><br><br>
-              <a class="link" href="grid3/index.php">Grid 3</a><br><br>
-              <a class="link" href="grid4/index.php">Grid 4</a><br><br>
-              <a class="link" href="grid5/index.html">Grid 5</a><br><br>
-              <a class="link" href="grid6/index.html">Grid 6</a><br><br>
-              <a class="link" href="grid7/index.html">Grid 7</a><br><br>
-              <a class="link" href="grid8/index.html">Grid 8</a><br><br>
-              <a class="link" href="grid9/index.html">Grid 9</a><br><br>
-              <a class="link" href="grid10/index.html">Grid 10</a><br><br>
-              <a class="link" href="grid11/index.html">Grid 11</a><br><br>
-              <a class="link" href="grid12/index.html">Grid 12</a><br><br>
+              <a class="link" href="/index.php">Menu</a><br><br>
+              <a class="link" href="/cssgrid/grid1/index.php">Grid 1</a><br><br>
+              <a class="link" href="/cssgrid/grid2/index.php">Grid 2</a><br><br>
+              <a class="link" href="/cssgrid/grid3/index.php">Grid 3</a><br><br>
+              <a class="link" href="/cssgrid/grid4/index.php">Grid 4</a><br><br>
+              <a class="link" href="/cssgrid/grid5/index.html">Grid 5</a><br><br>
+              <a class="link" href="/cssgrid/grid6/index.html">Grid 6</a><br><br>
+              <a class="link" href="/cssgrid/grid7/index.html">Grid 7</a><br><br>
+              <a class="link" href="/cssgrid/grid8/index.html">Grid 8</a><br><br>
+              <a class="link" href="/cssgrid/grid9/index.html">Grid 9</a><br><br>
+              <a class="link" href="/cssgrid/grid10/index.html">Grid 10</a><br><br>
+              <a class="link" href="/cssgrid/grid11/index.html">Grid 11</a><br><br>
+              <a class="link" href="/cssgrid/grid12/index.html">Grid 12</a><br><br>
         </div>
       <header>
       <?php
