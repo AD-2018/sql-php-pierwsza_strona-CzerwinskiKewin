adc38c61c9cb53a3764a64ef783aba03c2bdc500
Czerwinski-Kewin <czerwinski.kewin1@gmail.com>
2021-04-19 09:10:49 +0200

    menu1

---
+++ menu.php
@@ -0,0 +1,17 @@
+<?php
+echo('<div class="linki">'); 
+    echo('<div class="diw">');
+        echo("<br>");
+        echo("<br>");
+        echo("<br>");
+        echo('<a class="link" href="/pracownicy/pracownicyOrganizacja.php">Pracownicy i Organizacja</a><br><br>');
+        echo('<a class="link" href="/pracownicy/funkcjeAgregujace.php">Funkcje Agregujące</a><br><br>');
+        echo('<a class="link" href="/pracownicy/dataCzas.php">Data i Czas</a><br><br>');
+        echo('<a class="link" href="/pracownicy/formularz.html">Formularz</a><br><br>');
+        echo('<a class="link" href="/pracownicy/daneDoBazy.php">Dane do Bazy</a><br><br>');
+        echo('<a class="link" href="/ksiazki/biblioteka.php">Biblioteka</a><br><br>');
+        echo('<a class="link" href="/flexbox/index.html">Flexbox</a><br><br>');
+        echo('<a class="link" href="/cssgrid/index.html">Grid</a><br><br>');
+    echo('</div>');
+echo('</div>');
+?>
