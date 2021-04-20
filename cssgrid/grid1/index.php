f30765139e4ae677c0cbaf7b2a8b54305c913fb7
Czerwinski-Kewin <czerwinski.kewin1@gmail.com>
2021-04-20 12:30:57 +0200

    del1

--- cssgrid/grid1/index.php
+++ cssgrid/grid1/index.php
@@ -38,12 +38,19 @@
             echo("<br>");
             echo($sql);
             echo('<table border="1">');
-            echo('<th>ID producenta</th><th>Producent</th>');
+            echo('<th>ID producenta</th><th>Producent</th><th>Usun</th>');
 
             while($wiersz=mysqli_fetch_assoc($wynik))
             {
                 echo('<tr>');
-                echo('<td>'.$wiersz['id_producent'].'</td>'.'<td>'.$wiersz['producent'].'</td>');
+                echo('<td>'.$wiersz['id_producent'].'</td>'.'<td>'.$wiersz['producent'].'</td>'.'<td>
+	    
+                <form action="delproducent.php" method="POST">
+                  <input type="text" name="id" value="'.$wiersz['id_producent'].'" hidden>
+                  <input type="submit" value="X">
+                </form>
+                
+                </td>');
                 echo('</tr>');
             }
 
@@ -67,12 +74,19 @@
             echo("<br>");
             echo($sql);
             echo('<table border="1">');
-            echo('<th>ID artykulu</th><th>Artykul</th>');
+            echo('<th>ID artykulu</th><th>Artykul</th><th>Usun</th>');
 
             while($wiersz=mysqli_fetch_assoc($wynik))
             {
                 echo('<tr>');
-                echo('<td>'.$wiersz['id_artykul'].'</td>'.'<td>'.$wiersz['artykul'].'</td>');
+                echo('<td>'.$wiersz['id_artykul'].'</td>'.'<td>'.$wiersz['artykul'].'</td>'.'<td>
+	    
+                <form action="delartykul.php" method="POST">
+                  <input type="text" name="id" value="'.$wiersz['id_artykul'].'" hidden>
+                  <input type="submit" value="X">
+                </form>
+                
+                </td>');
                 echo('</tr>');
             }
 
@@ -92,12 +106,19 @@
             echo("<br>");
             echo($sql);
             echo('<table border="1">');
-            echo('<th>ID</th><th>Producent</th><th>Artykul</th>');
+            echo('<th>ID</th><th>Producent</th><th>Artykul</th><th>Usun</th>');
 
             while($wiersz=mysqli_fetch_assoc($wynik))
             {
                 echo('<tr>');
-                echo('<td>'.$wiersz['id_sklep'].'</td>'.'<td>'.$wiersz['producent'].'</td>'.'<td>'.$wiersz['artykul'].'</td>');
+                echo('<td>'.$wiersz['id_sklep'].'</td>'.'<td>'.$wiersz['producent'].'</td>'.'<td>'.$wiersz['artykul'].'</td>'.'<td>
+	    
+                <form action="delsklep.php" method="POST">
+                  <input type="text" name="id" value="'.$wiersz['id_sklep'].'" hidden>
+                  <input type="submit" value="X">
+                </form>
+                
+                </td>');
                 echo('</tr>');
             }
 
