3aa2ed9af4654413310fac3a3c4893e4bc58e16f
Czerwinski-Kewin <czerwinski.kewin1@gmail.com>
2021-04-20 12:41:41 +0200

    del2

--- cssgrid/grid1/index.php
+++ cssgrid/grid1/index.php
@@ -128,7 +128,25 @@
       ?>
       </aside>
       <footer>
-        4
+        <?php
+          echo('<h3>Usun producenta</h3>');
+          echo('<form action="delproducent.php" method="POST">');
+          echo('<label>ID producenta</label><input type="number" name="id"></br>');
+          echo('<input type="submit" value="Usuń">');
+          echo('</form>');
+
+          echo('<h3>Usun artykul</h3>');
+          echo('<form action="delartykul.php" method="POST">');
+          echo('<label>ID artykulu</label><input type="number" name="id"></br>');
+          echo('<input type="submit" value="Usuń">');
+          echo('</form>');
+
+          echo('<h3>Usun sklep</h3>');
+          echo('<form action="delsklep.php" method="POST">');
+          echo('<label>ID sklep</label><input type="number" name="id"></br>');
+          echo('<input type="submit" value="Usuń">');
+          echo('</form>');
+        ?>
       </footer>
     </div>
   </body>
