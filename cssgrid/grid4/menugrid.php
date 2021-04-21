7752a54dca78f5056cf14d24fd86d3c47f3cfe67
Czerwinski-Kewin <czerwinski.kewin1@gmail.com>
2021-04-21 14:21:00 +0200

    del4

---
+++ cssgrid/grid4/menugrid.php
@@ -0,0 +1,19 @@
+<?php
+echo('<div class="linki">');
+    echo('<ul class="gridLista">');
+        echo('<li class="gridPrzycisk"><a class="link" href="/index.php">Menu</a>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid1/index.php">Grid 1</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid2/index.php">Grid 2</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid3/index.php">Grid 3</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid4/index.php">Grid 4</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid5/index.html">Grid 5</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid6/index.html">Grid 6</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid7/index.html">Grid 7</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid8/index.html">Grid 8</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid9/index.html">Grid 9</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid10/index.html">Grid 10</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid11/index.html">Grid 11</a></li>');
+        echo('<li class="gridPrzycisk"><a class="link" href="/cssgrid/grid12/index.html">Grid 12</a></li>');
+    echo('</ul>');
+echo('</div>');
+?>
